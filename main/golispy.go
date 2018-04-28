d16a0d363bf188b4a4da3b51c6bdf59573e3f23b
Hector Urtubia <urtubia@gmail.com>
2018-04-28 14:03:00 -0700

    Adding very basic standalone interpreter

---
+++ main/golispy.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"golispy"
+	"os"
+	"io/ioutil"
+	"fmt"
+)
+
+func main() {
+	args := os.Args[1:]
+	fileString, error := ioutil.ReadFile(args[0])
+	if error != nil {
+		fmt.Fprintf(os.Stderr, "Error reading input file %s", fileString)
+		return
+	}
+	env := golispy.DefaultEnv()
+	parsedExp, _ := golispy.Parse(string(fileString))
+	result := golispy.Eval(parsedExp, env)
+	fmt.Println(result)
+}
